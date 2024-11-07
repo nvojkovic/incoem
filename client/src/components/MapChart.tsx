23dbb48534fd5c20201684940006fb2bbd4dc758
Nikola Vojkovic <nikola.vojkovic@live.com>
2024-11-07 17:47:06 +0100

    tes

--- client/src/components/MapChart.tsx
+++ client/src/components/MapChart.tsx
@@ -14,49 +14,53 @@ const MapChart = ({ settings, client, print }: MapChartProps) => {
   const incomes = settings.data.incomes.filter((inc) => inc.enabled);
   const startYear = new Date().getFullYear();
   return (
-    <StackedAreaChart
-      years={yearRange(startYear, startYear + settings.maxYearsShown - 1)}
-      spending={false}
-      initialHeight={print ? 700 : undefined}
-      lineData={
-        client.needsFlag
-          ? yearRange(startYear, startYear + settings.maxYearsShown - 1).map(
-            (currentYear) =>
-              calculateSpendingYear(
-                settings.data,
-                client.spending,
-                { ...settings, taxType: "Pre-Tax" },
-                currentYear,
-              ),
-          )
-          : []
-      }
-      stability={client.stabilityRatioFlag}
-      needsFlag={client.needsFlag}
-      stackedData={incomes.map((income, i) => ({
-        name: title(incomes, settings.data.people, i),
-        stable: income.stable,
-        values: yearRange(
-          startYear,
-          startYear + settings.maxYearsShown - 1,
-        ).map((year) =>
-          Math.round(
-            calculate({
-              people: settings.data.people,
-              income,
-              startYear,
-              currentYear: year,
-              deathYears: settings.deathYears as any,
-              dead: settings.whoDies,
-              inflation: settings.inflation,
-              incomes: incomes,
-              ssSurvivorAge: settings.ssSurvivorAge,
-              inflationType: settings.inflationType,
-            }).amount,
+    <div>
+      <div>{JSON.stringify(settings)}</div>
+      <div>{JSON.stringify(client)}</div>
+      <StackedAreaChart
+        years={yearRange(startYear, startYear + settings.maxYearsShown - 1)}
+        spending={false}
+        initialHeight={print ? 700 : undefined}
+        lineData={
+          client.needsFlag
+            ? yearRange(startYear, startYear + settings.maxYearsShown - 1).map(
+                (currentYear) =>
+                  calculateSpendingYear(
+                    settings.data,
+                    client.spending,
+                    { ...settings, taxType: "Pre-Tax" },
+                    currentYear,
+                  ),
+              )
+            : []
+        }
+        stability={client.stabilityRatioFlag}
+        needsFlag={client.needsFlag}
+        stackedData={incomes.map((income, i) => ({
+          name: title(incomes, settings.data.people, i),
+          stable: income.stable,
+          values: yearRange(
+            startYear,
+            startYear + settings.maxYearsShown - 1,
+          ).map((year) =>
+            Math.round(
+              calculate({
+                people: settings.data.people,
+                income,
+                startYear,
+                currentYear: year,
+                deathYears: settings.deathYears as any,
+                dead: settings.whoDies,
+                inflation: settings.inflation,
+                incomes: incomes,
+                ssSurvivorAge: settings.ssSurvivorAge,
+                inflationType: settings.inflationType,
+              }).amount,
+            ),
           ),
-        ),
-      }))}
-    />
+        }))}
+      />
+    </div>
   );
 };
 
