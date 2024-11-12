f06ee98da68232ffa37f959ddfd82881947107bd
Nikola Vojkovic <nikola.vojkovic@live.com>
2024-11-12 21:35:33 +0100

    fix spending

--- client/src/components/Spending/SpendingPage.tsx
+++ client/src/components/Spending/SpendingPage.tsx
@@ -21,7 +21,84 @@ export const calculateSpendingYear = (
   settings: ScenarioSettings,
   year: number,
 ) => {
-  console.log("spending", spending);
+  if (!spending) return 0;
+  const years = year - 2024;
+
+  const inflationRate = (inflation: YearlyIncrease) => {
+    if (!inflation || inflation.type == "none") return 0;
+    else if (inflation.type == "custom") return (inflation.percent || 0) / 100;
+    else if (inflation.type == "general") return settings.inflation / 100 || 0;
+    else return -10e9;
+  };
+
+  const inflateAmount = (amount: number, inflation: YearlyIncrease) => {
+    return amount * Math.pow(1 + inflationRate(inflation), years);
+  };
+
+  // back out existing spending
+  const existing = spending.preSpending
+    .map((item) => item.amount || 0)
+    .reduce((a, b) => a + b, 0);
+
+  let result = spending.currentSpending - existing;
+
+  //back out death reduction
+  if (settings.whoDies != -1 && settings.deathYears[settings.whoDies]) {
+    const age =
+      calculateAge(new Date(data.people[settings.whoDies].birthday)) + years;
+    if (age > (settings.deathYears[settings.whoDies] as any)) {
+      result = result * (1 - spending.decreaseAtDeath[settings.whoDies] / 100);
+    }
+  }
+  // inflate by general
+  result = inflateAmount(result, spending.yearlyIncrease);
+
+  // add pre back in
+  const pre = spending.preSpending
+    ?.filter((item) => year <= item.endYear)
+    .map((item) => inflateAmount(item.amount || 0, item.increase))
+    .reduce((a, b) => a + b, 0);
+  result += pre;
+
+  // add post spending
+  const post = spending.postSpending
+    ?.filter(
+      (item) => (item.endYear || 2100) >= year && (item.startYear || 0) <= year,
+    )
+    .map((item) => {
+      let amount = item.amount || 0;
+      if (settings.whoDies !== -1 && settings.deathYears[settings.whoDies]) {
+        const age =
+          calculateAge(new Date(data.people[settings.whoDies].birthday)) +
+          years;
+        if (age > (settings.deathYears[settings.whoDies] as any)) {
+          amount *= 1 - item.changeAtDeath[settings.whoDies] / 100;
+        }
+      }
+      return inflateAmount(amount, item.increase);
+    })
+    .reduce((a, b) => a + b, 0);
+  result += post;
+
+  if (settings.taxType == "Pre-Tax") {
+    if (year <= (settings.retirementYear || 0)) {
+      result /= 1 - (spending.preTaxRate || 0) / 100;
+    } else {
+      result /= 1 - (spending.postTaxRate || 0) / 100;
+    }
+  }
+  if (settings.inflationType == "Real") {
+    result = calculatePV(result, (settings.inflation || 0) / 100, years);
+  }
+
+  return Math.round(isNaN(result) ? 0 : result);
+};
+export const calculateSendingYear = (
+  data: IncomeMapData,
+  spending: RetirementSpendingSettings | undefined,
+  settings: ScenarioSettings,
+  year: number,
+) => {
   if (!spending) return 0;
   const years = year - 2024;
   let result = 0;
@@ -31,9 +108,11 @@ export const calculateSpendingYear = (
     else if (inflation.type == "general") return settings.inflation / 100 || 0;
     else return -10e9;
   };
+
   const inflateAmount = (amount: number, inflation: number) => {
     return amount * Math.pow(1 + inflation, years);
   };
+
   const pre = spending.preSpending
     ?.filter((item) => year > item.endYear)
     .map((item) =>
@@ -71,11 +150,9 @@ export const calculateSpendingYear = (
   result += post;
 
   let current = spending.currentSpending;
-  console.log("WHO", settings.whoDies);
   if (settings.whoDies != -1 && settings.deathYears[settings.whoDies]) {
     const age =
       calculateAge(new Date(data.people[settings.whoDies].birthday)) + years;
-    console.log(year, result, current, uninflatedPre, inflatedPre);
     if (age > (settings.deathYears[settings.whoDies] as any)) {
       current =
         (current + result) * 1 -
