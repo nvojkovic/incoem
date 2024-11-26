f34c84884c3ccdd5f50a4bd4cb534c66045b03c7
Nikola Vojkovic <nikola.vojkovic@live.com>
2024-11-26 07:36:29 +0100

    fix

--- client/src/useData.tsx
+++ client/src/useData.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useContext } from "react";
 import { updateData, updateScenarios } from "./services/client";
 import { updateAtIndex } from "./utils";
