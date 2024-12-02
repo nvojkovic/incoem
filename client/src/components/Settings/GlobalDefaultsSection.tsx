0666854ee5e1c17d338e6e160aa89d068724d06c
Nikola Vojkovic <nikola.vojkovic@live.com>
2024-12-02 16:53:56 +0100

    fix

--- client/src/components/Settings/GlobalDefaultsSection.tsx
+++ client/src/components/Settings/GlobalDefaultsSection.tsx
@@ -1,5 +1,4 @@
 import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
-import Button from "../Inputs/Button";
 import Input from "../Inputs/Input";
 import { SettingsData } from "./types";
 import { Tooltip } from "flowbite-react";
