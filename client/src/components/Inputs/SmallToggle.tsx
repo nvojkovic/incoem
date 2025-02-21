8ddcacda91cdd353151cbd21520c371ea201f1d9
Nikola Vojkovic <nikola.vojkovic@live.com>
2025-02-21 22:17:32 +0100

    fix versatile

--- client/src/components/Inputs/SmallToggle.tsx
+++ client/src/components/Inputs/SmallToggle.tsx
@@ -1,15 +1,15 @@
-const SmallToggle = ({ item1, item2, active, toggle }: any) => {
+const SmallToggle = ({ item1, item2, active, toggle, disabled }: any) => {
   return (
     <div className="flex text-sm cursor-pointer border-collapse">
       <div
         className={`${active === item1 ? "bg-main-orange text-white" : "bg-gray-200 text-black"} px-3 py-[3px] rounded-l-md  border  border-gray-300 border-1`}
-        onClick={toggle}
+        onClick={!disabled && toggle}
       >
         {item1}
       </div>
       <div
         className={`${active === item2 ? "bg-main-orange text-white" : "bg-gray-200 text-black"} px-3 py-[3px] rounded-r-md ml-[-1px] border  border-gray-300 border-1`}
-        onClick={toggle}
+        onClick={!disabled && toggle}
       >
         {item2}
       </div>
@@ -17,24 +17,31 @@ const SmallToggle = ({ item1, item2, active, toggle }: any) => {
   );
 };
 
-export const SmallTroggle = ({ item1, item2, item3, active, set }: any) => {
+export const SmallTroggle = ({
+  item1,
+  item2,
+  item3,
+  active,
+  set,
+  disabled,
+}: any) => {
   return (
     <div className="flex text-sm cursor-pointer border-collapse">
       <div
         className={`${active === item1 ? "bg-main-orange text-white" : "bg-gray-200 text-black"} px-3 py-[3px] rounded-l-md  border  border-gray-300 border-1`}
-        onClick={() => set(item1)}
+        onClick={() => !disabled && set(item1)}
       >
         {item1}
       </div>
       <div
         className={`${active === item2 ? "bg-main-orange text-white" : "bg-gray-200 text-black"} px-3 py-[3px]  ml-[-1px] border  border-gray-300 border-1`}
-        onClick={() => set(item2)}
+        onClick={() => !disabled && set(item2)}
       >
         {item2}
       </div>
       <div
         className={`${active === item3 ? "bg-main-orange text-white" : "bg-gray-200 text-black"} px-3 py-[3px] rounded-r-md ml-[-1px] border  border-gray-300 border-1`}
-        onClick={() => set(item3)}
+        onClick={() => !disabled && set(item3)}
       >
         {item3}
       </div>
